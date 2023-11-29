5b4e4d11d42453acee9702a8d02ad0a3357f3da0
thibault <thibault.henrion@gmail.com>
2023-11-29 15:40:48 +0100

    optimize insert

--- java/src/main/java/info/dmerej/contacts/Database.java
+++ java/src/main/java/info/dmerej/contacts/Database.java
@@ -37,19 +37,33 @@ public class Database {
 
     public void insertContacts(Stream<Contact> contacts) {
         try {
+            connection.setAutoCommit(false);
+
             PreparedStatement statement = connection.prepareStatement("""
                 INSERT INTO contacts VALUES (?, ? ,?)
             """);
+
             contacts.forEach(contact -> {
                 try {
                     statement.setInt(1, ++insertedCount);
                     statement.setString(2, contact.name());
                     statement.setString(3, contact.email());
-                    statement.executeUpdate();
+                    statement.addBatch();
+
+                    if (insertedCount % 1000 == 0) {
+                        statement.executeBatch();
+                        connection.commit();
+                        statement.clearBatch();
+                    }
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
             });
+
+            statement.executeBatch();
+            connection.commit();
+
+            connection.setAutoCommit(true);
         } catch (SQLException e ) {
             throw new RuntimeException(e);
         }
